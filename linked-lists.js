85f42cb57164c2d0030f8fa1d0ed02f12f2dbe31
Douglas Miner <curr3nas@gmail.com>
2020-05-07 11:29:36 -0500

    finished drill 2

--- linked-lists.js
+++ linked-lists.js
@@ -1,14 +1,11 @@
-class _Node{
+class _Node {
   constructor(value = null, next = null) {
     this.value = value;
     this.next = next;
   }
 }
 
-function main(value) {
-  let SLL = new _Node
-  
-}
+
 
 class LinkedList {
   constructor() {
@@ -28,9 +25,59 @@ class LinkedList {
       tempNode.next = new _Node(item, null);
     }
   }
+  insertBefore(item, key) {
+    if (this.head === null) {
+      this.insertFirst(item);
+      return;
+    }
+
+    let currNode = this.head;
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== key)) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    previousNode.next = new _Node(item, currNode);
+  }
+  insertAfter(item, key) {
+    if (this.head === null) {
+      this.insertFirst(item);
+      return;
+    }
+
+    let currNode = this.head;
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (previousNode.value !== key)) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    previousNode.next = new _Node(item, currNode);
+  }
+  insertAt(item, key) {
+    if (this.head === null) {
+      this.insertFirst(item);
+      return;
+    }
+
+    let currNode = this.head;
+    let previousNode = this.head;
+
+    for (let i = 1; i < parseInt(key); i++) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+
+    // while ((currNode !== null) && (previousNode.value !== key)) {
+    //   previousNode = currNode;
+    //   currNode = currNode.next;
+    // }
+    previousNode.next = new _Node(item, currNode);
+  }
   find(item) {
     let currNode = this.head;
-    if(!this.head) {
+    if (!this.head) {
       return null;
     }
     while (currNode.value !== item) {
@@ -64,3 +111,28 @@ class LinkedList {
     previousNode.next = currNode.next;
   }
 }
+
+function main() {
+  let SLL = new LinkedList;
+
+  SLL.insertFirst('Apollo');
+  SLL.insertFirst('Boomer');
+  SLL.insertFirst('Helo');
+  SLL.insertFirst('Husker');
+  SLL.insertFirst('Starbuck');
+  SLL.insertFirst('Tauhida');
+
+  SLL.insertBefore('Athena', 'Boomer');
+
+  SLL.insertAfter('Hotdog', 'Helo');
+
+  SLL.insertAt('Kat', 3)
+
+  SLL.remove('Tauhida');
+
+  console.log(SLL.find('Starbuck'));
+
+  return SLL;
+}
+
+console.log(main());
